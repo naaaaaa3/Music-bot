cca3cab997af135ac5b6fda378dae33aef58498a
naaaaaa3 <90862673+naaaaaa3@users.noreply.github.com>
2021-09-17 00:49:59 +0700

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     },
 
     discord: {
-        token: 'TOKEN',
-        prefix: 'PREFIX',
-        activity: 'ACTIVITY',
+        token: 'ODg4MTA5NDkyNTgyMTIxNTUz.YUN6aw.nPP-u_cM7zOYAVkQdm_KjA-BvwA',
+        prefix: 'z',
+        activity: 'CZ Music 2',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
